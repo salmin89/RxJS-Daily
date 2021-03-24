fc52f0bfe3682a537cbf725712b51c1e354bf8f2
Salmin Skenderovic <sskenderovic@proofpoint.com>
2021-03-24 13:46:32 -0400

    day 2

--- src/app/day2/day2.component.ts
+++ src/app/day2/day2.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-day2',
   templateUrl: './day2.component.html',
-  styleUrls: ['./day2.component.scss']
+  styleUrls: ['./day2.component.scss'],
 })
 export class Day2Component implements OnInit {
-
-  constructor() { }
+  visible$: Observable<boolean>;
+  constructor() {}
 
   ngOnInit(): void {
+    this.visible$ = fromEvent(document, 'visibilitychange').pipe(map(() => document['hidden']));
+    this.visible$.subscribe(console.log);
   }
-
 }
