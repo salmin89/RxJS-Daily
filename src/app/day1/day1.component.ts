1363147941b60dbc729b526e3fe192b82c01d822
Salmin Skenderovic <sskenderovic@proofpoint.com>
2021-03-24 11:58:26 -0400

    day1

--- src/app/day1/day1.component.ts
+++ src/app/day1/day1.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-day1',
@@ -6,7 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./day1.component.scss'],
 })
 export class Day1Component implements OnInit {
-  constructor() {}
+  @ViewChild('focusGroup') focusGroup: ElementRef;
+
+  selected$ = new Subject<any>();
+  children: any[];
+
+  constructor() {
+    this.selected$.subscribe(console.log);
+  }
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.children = this.getKeyboardFocusableElements(this.focusGroup.nativeElement);
+
+    this.children.forEach((child) => {
+      child.addEventListener('blur', this.removeSelected.bind(this));
+      child.addEventListener('focus', this.setSelected.bind(this));
+    });
+  }
+
+  removeSelected() {
+    this.selected$.next(null);
+  }
+  setSelected(e) {
+    this.selected$.next(e.target);
+  }
+
+  ngOnDestroy() {
+    this.children.forEach((child) => {
+      child.removeEventListener('blur', this.removeSelected.bind(this));
+      child.removeEventListener('focus', this.setSelected.bind(this));
+    });
+
+    this.selected$.complete();
+  }
+
+  getKeyboardFocusableElements(element) {
+    return [...element.querySelectorAll('a, button, input, textarea, select, details,[tabindex]:not([tabindex="-1"])')].filter(
+      (el) => !el.hasAttribute('disabled')
+    );
+  }
 }
