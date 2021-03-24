e54ff8080fed7e01913506d49484ac1e9b8915d7
Salmin Skenderovic <sskenderovic@proofpoint.com>
2021-03-24 09:48:47 -0400

    added more routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Day1Component } from './day1/day1.component';
+import { Day2Component } from './day2/day2.component';
+import { Day3Component } from './day3/day3.component';
 
 const routes: Routes = [
   {
     path: 'day1',
     component: Day1Component,
+  },
+  {
+    path: 'day2',
+    component: Day2Component,
+  },
+  {
+    path: 'day3',
+    component: Day3Component,
   },
 ];
 
