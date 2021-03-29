4f59941e62259ea02a4ab8b0ef37bdd3df154c8d
Salmin Skenderovic <sskenderovic@proofpoint.com>
2021-03-29 09:18:10 -0400

    day5 done

--- src/app/day5/day5.component.ts
+++ src/app/day5/day5.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { timer, interval, BehaviorSubject } from 'rxjs';
+import { map, takeWhile, withLatestFrom, switchMap, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-day5',
   templateUrl: './day5.component.html',
-  styleUrls: ['./day5.component.scss']
+  styleUrls: ['./day5.component.scss'],
 })
 export class Day5Component implements OnInit {
+  start = new BehaviorSubject(10);
 
-  constructor() { }
+  countdown$ = this.start.pipe(
+    switchMap((start) =>
+      interval(1000).pipe(
+        map((num) => start - num),
+        takeWhile((current) => current >= 0)
+      )
+    ),
+    shareReplay()
+  );
 
-  ngOnInit(): void {
+  constructor() {
+    this.countdown$.subscribe(console.log);
   }
 
+  ngOnInit(): void {}
 }
