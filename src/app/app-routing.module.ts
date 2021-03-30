1ab7bb55900a4f36220d49a291bdd489f518d714
Salmin Skenderovic <sskenderovic@proofpoint.com>
2021-03-30 10:26:06 -0400

    Added day7 template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,9 @@ import { Day3Component } from './day3/day3.component';
 import { Day4Component } from './day4/day4.component';
 import { Day5Component } from './day5/day5.component';
 import { Day6Component } from './day6/day6.component';
+import { Day7Component } from './day7/day7.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     path: 'day1',
     component: Day1Component,
@@ -31,6 +32,10 @@ const routes: Routes = [
   {
     path: 'day6',
     component: Day6Component,
+  },
+  {
+    path: 'day7',
+    component: Day7Component,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { Day1Component } from './day1/day1.component';
 import { Day2Component } from './day2/day2.component';
 import { Day3Component } from './day3/day3.component';
 import { Day4Component } from './day4/day4.component';
 import { Day5Component } from './day5/day5.component';
-import { LoadingService } from './loading.service';
 import { Day6Component } from './day6/day6.component';
+import { Day7Component } from './day7/day7.component';
 
+import { LoadingService } from './loading.service';
 import { TuiRepeatTimesModule } from '@taiga-ui/cdk';
 @NgModule({
   imports: [BrowserModule, AppRoutingModule, TuiRepeatTimesModule],
-  declarations: [AppComponent, Day1Component, Day2Component, Day3Component, Day4Component, Day5Component, Day6Component],
+  declarations: [AppComponent, Day1Component, Day2Component, Day3Component, Day4Component, Day5Component, Day6Component, Day7Component],
   providers: [LoadingService],
   bootstrap: [AppComponent],
 })
