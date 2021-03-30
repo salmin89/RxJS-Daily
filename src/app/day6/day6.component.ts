ea30b67af817b826d3d8be029f0aa300a09536b0
Salmin Skenderovic <sskenderovic@proofpoint.com>
2021-03-30 10:19:19 -0400

    day6 done

--- src/app/day6/day6.component.ts
+++ src/app/day6/day6.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { shareReplay, startWith, map, withLatestFrom, scan, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-day6',
   templateUrl: './day6.component.html',
-  styleUrls: ['./day6.component.scss']
+  styleUrls: ['./day6.component.scss'],
 })
 export class Day6Component implements OnInit {
+  select$: Subject<number> = new Subject();
+  selected$ = this.select$.pipe(
+    scan((acc, curr) => {
+      acc[curr] = !acc[curr];
+      return acc;
+    }, {}),
+    startWith({}),
+    shareReplay()
+  );
 
-  constructor() { }
+  buyBtnClick$ = new Subject();
 
-  ngOnInit(): void {
+  purchase$ = this.buyBtnClick$.pipe(
+    withLatestFrom(this.selected$),
+    map(([, selected]) => selected),
+    tap(() => this.select$.complete())
+  );
+
+  finalCost$ = this.purchase$.pipe(map((selected) => this.getValues(selected).length * 3));
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  getValues(selectedMap) {
+    return Object.keys(selectedMap).filter((key) => selectedMap[key]);
   }
-
 }
