3482509caf2cf9e60b1d47b5b5bb0f3218ed95df
Salmin Skenderovic <sskenderovic@proofpoint.com>
2021-04-05 09:36:59 -0400

    day7 done

--- src/app/day7/day7.component.ts
+++ src/app/day7/day7.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map, filter, debounceTime, distinct, startWith, throttleTime, distinctUntilChanged, pairwise } from 'rxjs/operators';
+import { DOCUMENT } from '@angular/common';
+
+const THRESHOLD = 500;
 
 @Component({
   selector: 'app-day7',
   templateUrl: './day7.component.html',
-  styleUrls: ['./day7.component.scss']
+  styleUrls: ['./day7.component.scss'],
 })
 export class Day7Component implements OnInit {
+  readonly hidden$ = fromEvent(this.documentRef, 'scroll').pipe(
+    throttleTime(50),
+    map(() => this.documentRef.documentElement.scrollTop),
+    pairwise(),
+    map(([prev, next]) => next > THRESHOLD || prev < next),
+    distinctUntilChanged()
+  );
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private readonly documentRef: Document) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
