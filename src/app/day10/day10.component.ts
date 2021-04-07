ff3e2ba5f1d64b72ec830b1f7195d0ee14f0947f
Salmin Skenderovic <sskenderovic@proofpoint.com>
2021-04-07 15:45:51 -0400

    day10 solution

--- src/app/day10/day10.component.ts
+++ src/app/day10/day10.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Inject } from '@angular/core';
+import { combineLatest, fromEvent, Observable, Subject } from 'rxjs';
+import { tap, debounceTime, throttleTime, mapTo, map, skipUntil, takeUntil, repeat } from 'rxjs/operators';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-day10',
   templateUrl: './day10.component.html',
-  styleUrls: ['./day10.component.scss']
+  styleUrls: ['./day10.component.scss'],
 })
 export class Day10Component implements OnInit {
+  PALETTE_WIDTH = 200;
+  PALETTE_HEIGHT = 200;
+  HUE = 197;
 
-  constructor() { }
+  @ViewChild('colorPicker') colorPicker: ElementRef<any>;
 
-  ngOnInit(): void {
+  mouseMove$: Observable<{ offsetX: number; offsetY: number }>;
+
+  constructor(@Inject(DOCUMENT) private readonly documentRef: Document) {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    const move$ = fromEvent(this.colorPicker.nativeElement, 'mousemove').pipe(
+      throttleTime(50),
+      map(({ offsetX, offsetY }) => ({ offsetX, offsetY }))
+    );
+    const down$ = fromEvent(this.colorPicker.nativeElement, 'mousedown').pipe(map(() => true));
+    const up$ = fromEvent(this.documentRef, 'mouseup').pipe(map(() => false));
+
+    this.mouseMove$ = move$.pipe(skipUntil(down$), takeUntil(up$), repeat());
+
+    this.mouseMove$.subscribe(console.log);
   }
-
 }
