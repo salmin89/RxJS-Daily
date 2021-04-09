43497d2d4cc421046ba7df767804016237cea3be
Salmin Skenderovic <sskenderovic@proofpoint.com>
2021-04-09 10:26:54 -0400

    day11 solution

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { Observer } from 'rxjs';
+import { Component, Inject, Input, ElementRef } from '@angular/core';
+import { Observer, fromEvent, timer } from 'rxjs';
+import { repeatWhen, takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'notification',
@@ -10,7 +11,19 @@ export class NotificationComponent<T> {
   @Input()
   observer: Observer<T>;
 
+  readonly mouseenter$ = fromEvent(this.elementRef.nativeElement, 'mouseenter');
+
+  readonly mouseleave$ = fromEvent(this.elementRef.nativeElement, 'mouseleave');
+
+  readonly close$ = timer(3000).pipe(
+    takeUntil(this.mouseenter$),
+    repeatWhen(() => this.mouseleave$),
+    tap(this.close.bind(this))
+  );
+
   close() {
     this.observer.complete();
   }
+
+  constructor(@Inject(ElementRef) private readonly elementRef: ElementRef<HTMLElement>) {}
 }
