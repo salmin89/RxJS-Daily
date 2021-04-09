c801b877626f7350a78bab03054341d23cd344e1
Salmin Skenderovic <sskenderovic@proofpoint.com>
2021-04-09 11:02:38 -0400

    my solution day 12

--- src/app/portal/portal.component.ts
+++ src/app/portal/portal.component.ts
@@ -1,5 +1,7 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { ModalService } from '../modal.service';
+import { combineLatest, fromEvent, Subscription } from 'rxjs';
+import { filter, takeWhile, switchMap, withLatestFrom, repeat, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-portal',
@@ -7,5 +9,38 @@ import { ModalService } from '../modal.service';
   styleUrls: ['./portal.component.scss'],
 })
 export class PortalComponent {
-  constructor(@Inject(ModalService) readonly modal$$: ModalService) {}
+  readonly esc$ = fromEvent(this.elementRef.nativeElement, 'keydown').pipe(filter((e: KeyboardEvent) => e.code === 'Escape'));
+
+  readonly outSideClick$ = fromEvent(this.elementRef.nativeElement, 'mousedown').pipe(
+    filter((e: MouseEvent) => e.target === this.overlay?.nativeElement)
+  );
+
+  @ViewChild('overlay') overlay: ElementRef<HTMLElement>;
+
+  constructor(
+    @Inject(ModalService) readonly modal$$: ModalService,
+    @Inject(ElementRef) private readonly elementRef: ElementRef<HTMLElement>
+  ) {}
+
+  ngOnInit() {
+    this.esc$
+      .pipe(
+        withLatestFrom(this.modal$$),
+        takeWhile(([esc, modal]) => modal !== null),
+        repeat()
+      )
+      .subscribe(([esc, modal]) => {
+        this.modal$$.next(null);
+      });
+
+    this.outSideClick$
+      .pipe(
+        withLatestFrom(this.modal$$),
+        takeWhile(([esc, modal]) => modal !== null),
+        repeat()
+      )
+      .subscribe(() => {
+        this.modal$$.next(null);
+      });
+  }
 }
